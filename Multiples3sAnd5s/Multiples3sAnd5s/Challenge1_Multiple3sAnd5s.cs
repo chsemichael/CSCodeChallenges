13047d1e5f0ee2926c86206c6a60c1d7ced66904
Chase Albaugh <cmcclainalbaugh@outlook.com>
2024-05-20 19:13:30 -0500

    Added and completed Multiples of 3s or 5s Challenge

---
+++ Multiples3sAnd5s/Multiples3sAnd5s/Challenge1_Multiple3sAnd5s.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Multiples3sAnd5s
+{
+    internal class Challenge1_Multiple3sAnd5s
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiples of 3s and 5s");
+            var sum = 0;
+            for (var i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                { 
+                    sum += i;
+                }
+                
+            }
+            Console.WriteLine("The sum of multiples of 3 or 5 is: " + sum);
+
+        }
+        
+    }
+}
